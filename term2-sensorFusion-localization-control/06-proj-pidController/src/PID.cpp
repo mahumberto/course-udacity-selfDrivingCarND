6f1eabdb153026ecab0ac136fc69fd9278f9b73c
hmaleman <ma.humberto@gmail.com>
2017-08-18 21:55:30 +0200

    feat: Update controller and tunned parameters

--- term2-sensorFusion-localization-control/06-proj-pidController/src/PID.cpp
+++ term2-sensorFusion-localization-control/06-proj-pidController/src/PID.cpp
@@ -2,20 +2,28 @@
 
 using namespace std;
 
-/*
-* TODO: Complete the PID class.
-*/
-
 PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(const double Kp, const double Ki, const double Kd) {
+  this->Kp = Kp;
+  p_error = 0;
+
+  this->Kd = Kd;
+  d_error = 0;
+
+  this->Ki = Ki;
+  i_error = 0;
 }
 
-void PID::UpdateError(double cte) {
+void PID::UpdateError(const double cte) {
+  const double delta_cte = cte - p_error;
+  p_error = cte;
+  d_error = delta_cte;
+  i_error += cte;
 }
 
 double PID::TotalError() {
+  return (-(Kp*p_error + Kd*d_error + Ki*i_error));
 }
-
--- term2-sensorFusion-localization-control/06-proj-pidController/src/main.cpp
+++ term2-sensorFusion-localization-control/06-proj-pidController/src/main.cpp
@@ -33,7 +33,16 @@ int main()
   uWS::Hub h;
 
   PID pid;
-  // TODO: Initialize the pid variable.
+  // Initialize the pid variable.
+  // The variables were tunned manually by try-error
+  // P gain - It was increased by 0.01 steps until the controller corrected the
+  //          steering in time for the turn. A small P gain would not correct
+  //          enough and the vehicle went off the track.
+  // D gain - It was increased by steps of 0.1 in order to decrease the oscilation
+  //          caused by the P gain.
+  // I gain - Since it was not necessary to correct the bias, no integral gain
+  //          was used.
+  pid.Init(0.14, 0., 0.85);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -51,13 +60,11 @@ int main()
           double speed = std::stod(j[1]["speed"].get<std::string>());
           double angle = std::stod(j[1]["steering_angle"].get<std::string>());
           double steer_value;
-          /*
-          * TODO: Calcuate steering value here, remember the steering value is
-          * [-1, 1].
-          * NOTE: Feel free to play around with the throttle and speed. Maybe use
-          * another PID controller to control the speed!
-          */
-          
+
+          //Calcuate steering value here
+          pid.UpdateError(cte);
+          steer_value = pid.TotalError();
+
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
